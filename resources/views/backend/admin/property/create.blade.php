ab107db896fd522b9119c1123f9efb801694f7c2
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-10-17 10:37:32 +0600

    multiple image choose & show in property.create

--- resources/views/backend/admin/property/create.blade.php
+++ resources/views/backend/admin/property/create.blade.php
@@ -63,7 +63,8 @@
                                 <div class="col-sm-6">
                                     <div class="mb-3">
                                         <label class="form-label">Multiple Image</label>
-                                        <input type="file" name="" class="form-control">
+                                        <input type="file" name="multi_img[]" class="form-control" id="multiImg" multiple="">
+                                        <div class="row" id="preview_img"></div>
                                     </div>
                                 </div>
                                 <!-- Col -->
@@ -112,6 +113,8 @@
     </div>
 
 @section('script')
+
+    {{-- Single Thumnail Image --}}
     <script>
         function mainThumbUrl(input){
             if (input.files && input.files[0]) {
@@ -122,6 +125,35 @@
                 reader.readAsDataURL(input.files[0]);
             }
         }
+    </script>
+
+    {{-- Multiple Image Choose --}}
+    <script>
+        $(document).ready(function(){
+        $('#multiImg').on('change', function(){ //on file input change
+            if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+            {
+                var data = $(this)[0].files; //this file data
+
+                $.each(data, function(index, file){ //loop though each file
+                    if(/(\.|\/)(gif|jpe?g|png|webp)$/i.test(file.type)){ //check supported file type
+                        var fRead = new FileReader(); //new filereader
+                        fRead.onload = (function(file){ //trigger function on successful read
+                        return function(e) {
+                            var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(100)
+                        .height(80); //create image element
+                            $('#preview_img').append(img); //append image to output element
+                        };
+                        })(file);
+                        fRead.readAsDataURL(file); //URL representing the file's data.
+                    }
+                });
+
+            }else{
+                alert("Your browser doesn't support File API!"); //if File API is absent
+            }
+        });
+        });
     </script>
 @endsection
 @endsection
