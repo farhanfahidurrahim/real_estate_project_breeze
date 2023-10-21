16b1b2412739478a59c928e054a55b7276cc33fe
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-10-21 12:20:56 +0600

    agent property store than user credit update

--- app/Http/Controllers/Agent/AgentPropertyController.php
+++ app/Http/Controllers/Agent/AgentPropertyController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Agent;
 
-use App\Http\Controllers\Controller;
+use App\Models\Type;
+use App\Models\User;
 use App\Models\Amenity;
 use App\Models\Facility;
-use App\Models\MultiImage;
 use App\Models\Property;
+use App\Models\MultiImage;
 use Illuminate\Support\Str;
-use App\Models\Type;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image;
 
@@ -32,7 +34,7 @@ class AgentPropertyController extends Controller
     {
         $types = Type::latest()->get();
         $amenities = Amenity::latest()->get();
-        return view('backend.agent.property.create', compact('types','amenities'));
+        return view('backend.agent.property.create', compact('types', 'amenities'));
     }
 
     /**
@@ -71,16 +73,16 @@ class AgentPropertyController extends Controller
         $slug = Str::of($request->property_name)->slug('-');
 
         $amenitiesArray = $request->amenities_id;
-        $amenitiesToString = implode(',',$amenitiesArray);
+        $amenitiesToString = implode(',', $amenitiesArray);
 
         $image = $request->file('property_thumbnail');
-        $imgName = $slug.'-'.date('dmY').'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(370,250)->save('upload/images/property/thumbnail/'.$imgName);
+        $imgName = $slug . '-' . date('dmY') . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(370, 250)->save('upload/images/property/thumbnail/' . $imgName);
 
         $propertyInsert = Property::create([
             'property_name' => $request->property_name,
             'property_slug' => $slug,
-            'property_code' => 'PC-'.Str::random(5),
+            'property_code' => 'PC-' . Str::random(5),
             'property_status' => $request->property_status,
             'lowest_price' => $request->lowest_price,
             'maximum_price' => $request->maximum_price,
@@ -112,8 +114,8 @@ class AgentPropertyController extends Controller
         //Miltiple Image Upload
         $image = $request->file('multi_img');
         foreach ($image as $img) {
-            $imageName = Str::random(5).'.'.$img->getClientOriginalExtension();
-            Image::make($img)->resize(770,520)->save('upload/images/property/multiple/'.$imageName);
+            $imageName = Str::random(5) . '.' . $img->getClientOriginalExtension();
+            Image::make($img)->resize(770, 520)->save('upload/images/property/multiple/' . $imageName);
 
             MultiImage::create([
                 'property_id' => $propertyInsert->id,
@@ -124,7 +126,7 @@ class AgentPropertyController extends Controller
         //Faciltities Add / Add More
         $facilities = Count($request->facility_name);
         if ($facilities != NULL) {
-            for ($i=0; $i < $facilities ; $i++) {
+            for ($i = 0; $i < $facilities; $i++) {
                 $data = new Facility();
                 $data->property_id = $propertyInsert->id;
                 $data->facility_name = $request->facility_name[$i];
@@ -132,6 +134,15 @@ class AgentPropertyController extends Controller
                 $data->save();
             }
         }
+
+        //Update User Table 'Credit'
+        $id = Auth::user()->id;
+        $userId = User::findOrFail($id);
+        $accessCredit = $userId->credit;
+
+        User::where('id', $id)->update([
+            'credit' => DB::raw('1 + '.$accessCredit),
+        ]);
 
         $notification = array(
             'message' => "Property Created Succesfully!",
